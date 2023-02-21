e777b72be775e2971238c9d14e1b4f434bc1ff75
MERT ANIL <107416952+mertanildk@users.noreply.github.com>
2023-02-22 00:27:24 +0300

    ParentAppClass implemented LOG
    Added Logger, we can see info on terminal

--- twitter-to-kafka-serv/src/main/java/twittertoKafkaService/TwitterToKafkaApplication.java
+++ twitter-to-kafka-serv/src/main/java/twittertoKafkaService/TwitterToKafkaApplication.java
@@ -1,15 +1,33 @@
 package twittertoKafkaService;
 
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import twittertoKafkaService.config.TwitterToKafkaServConfigData;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class TwitterToKafkaApplication implements CommandLineRunner {
-    public static void main(String[] args) {SpringApplication.run(TwitterToKafkaApplication.class, args);}
+    private final Logger LOG = LoggerFactory.getLogger(TwitterToKafkaApplication.class);
+    private final TwitterToKafkaServConfigData twitterToKafkaServConfigData;
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterToKafkaApplication.class, args);
+    }
+
     @Override
-    public void run(String... args) throws Exception {System.out.println("Application started");}
+    public void run(String... args) throws Exception {
+        LOG.info("TwitterToKafkaApplication is running");
+        LOG.info(Arrays.toString(twitterToKafkaServConfigData.getTwitterKeywords().toArray()));
+        LOG.info(twitterToKafkaServConfigData.getWelcomeMessage());
+    }
 
 
 
