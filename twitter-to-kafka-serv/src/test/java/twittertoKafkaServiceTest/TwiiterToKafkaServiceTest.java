deccea68fc63262c8e8e811855dca82ad7644c49
MERT ANIL <107416952+mertanildk@users.noreply.github.com>
2023-02-22 00:01:07 +0300

    Create TwiiterToKafkaServiceTest.java

---
+++ twitter-to-kafka-serv/src/test/java/twittertoKafkaServiceTest/TwiiterToKafkaServiceTest.java
@@ -0,0 +1,11 @@
+package twittertoKafkaServiceTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TwiiterToKafkaServiceTest {
+    @Test
+    void contextLoads() {
+    }
+}
