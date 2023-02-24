911c3b0bc73b3723bbcc595404b75cb1bdc53a11
MERT ANIL <107416952+mertanildk@users.noreply.github.com>
2023-02-24 21:07:31 +0300

    ComponentScan added
    com.microservices.demo path added modules' pattern

---
+++ twitter-to-kafka-serv/src/main/java/com/microservices/demo/twittertoKafkaService/TwitterToKafkaApplication.java
@@ -0,0 +1,45 @@
+package com.microservices.demo.twittertoKafkaService;
+
+import com.microservices.demo.twittertoKafkaService.runner.StreamRunner;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import com.microservices.demo.config.TwitterToKafkaServConfigData;
+
+import java.util.Arrays;
+
+
+@SpringBootApplication
+@RequiredArgsConstructor
+@ComponentScan(basePackages = "com.microservices.demo")
+//paketleri araması için ortak bir paket oluşturduk ve onu belirttik ve bu paketlerdeki classları bulup yükleyecek
+public class TwitterToKafkaApplication implements CommandLineRunner {
+    private final Logger LOG = LoggerFactory.getLogger(TwitterToKafkaApplication.class);
+    private final TwitterToKafkaServConfigData twitterToKafkaServConfigData;
+    private final StreamRunner streamRunner;
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterToKafkaApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        LOG.info("TwitterToKafkaApplication is running");
+        LOG.info(Arrays.toString(twitterToKafkaServConfigData.getTwitterKeywords().toArray()));
+        LOG.info(twitterToKafkaServConfigData.getWelcomeMessage());
+        streamRunner.run();
+    }
+
+
+
+    /*
+    @Override
+    public void onApplicationEvent(ApplicationEvent event) { //from   implements ApplicationListener and it will run one time when the application start
+    }
+     */
+
+}
