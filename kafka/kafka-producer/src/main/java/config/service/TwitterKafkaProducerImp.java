25dd03b3cb549db6ade407c63a544f6aad995f8e
MERT ANIL <107416952+mertanildk@users.noreply.github.com>
2023-03-01 23:38:43 +0300

    Imp class it will send message and write log

---
+++ kafka/kafka-producer/src/main/java/config/service/TwitterKafkaProducerImp.java
@@ -0,0 +1,58 @@
+package config.service;
+
+import example.avro.TwitterAvroModel;
+
+
+import jakarta.annotation.PreDestroy;
+import org.apache.http.concurrent.FutureCallback;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.assertj.core.internal.Futures;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+public class TwitterKafkaProducerImp implements KafkaProducer<Long, TwitterAvroModel> {
+    private final Logger LOG = LoggerFactory.getLogger(TwitterKafkaProducerImp.class);
+
+    private KafkaTemplate<Long, TwitterAvroModel> kafkaTemplate;
+
+    @Override
+    public void send(String topicName, Long key, TwitterAvroModel message) {
+
+        LOG.info("Sending message = {} to topic {}", message, topicName);
+
+        ListenableFuture<SendResult<Long, TwitterAvroModel>> kafkaResultFuture = kafkaTemplate.send(topicName, key, message);
+        CompletableFuture<SendResult<Long, TwitterAvroModel>> completableFuture = kafkaResultFuture.completable();
+        addCallBack(topicName, completableFuture);
+
+
+    }
+
+    @PreDestroy
+    public void close() {
+        if (kafkaTemplate != null) {
+            LOG.info("Closing Kafka Producer");
+            kafkaTemplate.destroy();
+        }
+    }
+
+    private void addCallBack(String topicName, CompletableFuture<SendResult<Long, TwitterAvroModel>> completableFuture) {
+        completableFuture.whenComplete((result, ex) -> {
+            if (ex != null) {
+                LOG.error("Error while sending message to topic {}", topicName, ex);
+            } else {
+                RecordMetadata recordMetadata = result.getRecordMetadata();
+                LOG.debug("Received new metadata. Topic: {}, Partition: {}, Offset: {}, Timestamp: {}",
+                        recordMetadata.topic(), recordMetadata.partition(), recordMetadata.offset(),
+                        recordMetadata.timestamp(), System.nanoTime());
+            }
+        });
+    }
+}
