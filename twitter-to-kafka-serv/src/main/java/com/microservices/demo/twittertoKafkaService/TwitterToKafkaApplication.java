24a0ab63f0a8477c1a28d6487047b854ec38b142
MERT ANIL <107416952+mertanildk@users.noreply.github.com>
2023-03-04 17:05:43 +0300

    Constructor Injection used.

--- twitter-to-kafka-serv/src/main/java/com/microservices/demo/twittertoKafkaService/TwitterToKafkaApplication.java
+++ twitter-to-kafka-serv/src/main/java/com/microservices/demo/twittertoKafkaService/TwitterToKafkaApplication.java
@@ -1,7 +1,6 @@
 package com.microservices.demo.twittertoKafkaService;
 
 import com.microservices.demo.twittertoKafkaService.runner.StreamRunner;
-import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -14,13 +13,17 @@ import java.util.Arrays;
 
 
 @SpringBootApplication
-@RequiredArgsConstructor
 @ComponentScan(basePackages = "com.microservices.demo")
 //paketleri araması için ortak bir paket oluşturduk ve onu belirttik ve bu paketlerdeki classları bulup yükleyecek
 public class TwitterToKafkaApplication implements CommandLineRunner {
     private final Logger LOG = LoggerFactory.getLogger(TwitterToKafkaApplication.class);
     private final TwitterToKafkaServConfigData twitterToKafkaServConfigData;
     private final StreamRunner streamRunner;
+
+    public TwitterToKafkaApplication(TwitterToKafkaServConfigData twitterToKafkaServConfigData, StreamRunner streamRunner) {
+        this.twitterToKafkaServConfigData = twitterToKafkaServConfigData;
+        this.streamRunner = streamRunner;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TwitterToKafkaApplication.class, args);
