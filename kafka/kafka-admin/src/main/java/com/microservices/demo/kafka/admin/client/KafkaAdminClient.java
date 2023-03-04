b37c113141bf161da8519dbb355cafadd9b0db06
MERT ANIL <107416952+mertanildk@users.noreply.github.com>
2023-03-04 17:43:10 +0300

    Updated

--- kafka/kafka-admin/src/main/java/com/microservices/demo/kafka/admin/client/KafkaAdminClient.java
+++ kafka/kafka-admin/src/main/java/com/microservices/demo/kafka/admin/client/KafkaAdminClient.java
@@ -1,16 +1,14 @@
-package kafka.admin.client;
+package com.microservices.demo.kafka.admin.client;
 
 import com.microservices.demo.config.KafkaConfigData;
 import com.microservices.demo.config.RetryConfigData;
-import kafka.admin.config.WebClientConfig;
-import kafka.admin.exception.KafkaClientException;
+import com.microservices.demo.kafka.admin.exception.KafkaClientException;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.CreateTopicsResult;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.admin.TopicListing;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -21,7 +19,6 @@ import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
-import java.net.http.HttpClient;
 import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
